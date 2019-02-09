5dfed903a737de114a4291b874911d65a3cf1bf6
Neugebauer-Jonas <neugebauer.dev@gmail.com>
2019-02-09 20:10:27 +0100

    dual timer

--- Gping.py
+++ Gping.py
@@ -14,26 +14,29 @@ while True:
         '192.168.1.1']
 
     for hostname in hostnames:
-        response = os.system('ping -n 4 ' + hostname)
+        response = os.system('ping -n 2 ' + hostname)
         if response == 0:
-            s = speedtest.Speedtest()
-            s.get_servers()
-            s.get_best_server()
-            s.download()
-            s.upload()
-            res = s.results.dict()
+            if hostname == '8.8.8.8':
+                s = speedtest.Speedtest()
+                s.get_servers()
+                s.get_best_server()
+                s.download()
+                s.upload()
+                res = s.results.dict()
                      
             print hostname, 'ok'
             with open("ok.txt", "a") as myfile:
                 myfile.write(str(datetime.now())+'\n    -->'+hostname+'-->up\n')
-                myfile.write('  Ping:      '+str(res["ping"])+'ms \n')
-                myfile.write('  Download:  '+str(res["download"]/1024)+'Kb/s \n')
-                myfile.write('  Upload:    '+str(res["upload"]/1024)+'Kb/s \n\n\n')
-               
+                if hostname == '8.8.8.8' :              
+                    myfile.write('  Ping:      '+str(res["ping"])+'ms \n')
+                    myfile.write('  Download:  '+str(res["download"]/1024)+'Kb/s \n')
+                    myfile.write('  Upload:    '+str(res["upload"]/1024)+'Kb/s \n\n\n')
+            print 'sleeping 5 minutes'
+            time.sleep(5*60)
         else:
             print hostname, 'DOWN'
             with open("down.txt", "a") as myfile:
                 myfile.write(str(datetime.now())+'\n    -->'+hostname+'-->pad veze \n')
-    time.sleep(120)
-    #Spavaj 2 minute
+            print 'sleeping 0,5 minutes'
+            time.sleep(30)
 	
