3f48dc882db8c19f8c85eb9bac599ba5aeac47ff
Neugebauer-Jonas <neugebauer.dev@gmail.com>
2019-02-10 19:08:24 +0100

    10/02

--- Gping.py
+++ Gping.py
@@ -3,40 +3,53 @@ import time
 from datetime import datetime 
 import speedtest   
     
-    
-    
+hostnames = ['192.168.1.1','1.1.1.1','213.191.128.8','213.191.128.9','8.8.8.8']    
+ #8.8.8.8-Google 
+    #192.168.1.1-Lokalni ruter    
 
 while True:   
-    #8.8.8.8-Google 
-    #192.168.1.1-Lokalni ruter 
-    hostnames = [
-        '8.8.8.8',
-        '192.168.1.1']
-
+    print 'sleeping 1 minutes'
+    time.sleep(60) 
+    print 'Starting Speedtest'	
+    try:
+        s = speedtest.Speedtest()
+        s.get_servers()
+        s.get_best_server()
+        s.download()
+        s.upload()
+        res = s.results.dict()
+        with open("speed.txt", "a") as myfile:	
+            myfile.write(str(datetime.now())+'\n')					
+            myfile.write('  Ping:      '+str(res["ping"])+'ms \n')
+            myfile.write('  Download:  '+str(res["download"]/1024)+'Kb/s \n')
+            myfile.write('  Upload:    '+str(res["upload"]/1024)+'Kb/s \n\n\n')
+            myfile.write('\n\n')  
+        print 'Speedtest Complete'				
+       
+    except:
+        print 'speedtest failed'
+        with open("down.txt", "a") as myfile:
+            myfile.write(str(datetime.now())+'    -->Speedtest-->pad veze \n')
+        print 'Sleep 1 minute'
+        
+        
     for hostname in hostnames:
-        response = os.system('ping -n 2 ' + hostname)
+        response = os.system('ping -n 4 ' + hostname)
         if response == 0:
-            if hostname == '8.8.8.8':
-                s = speedtest.Speedtest()
-                s.get_servers()
-                s.get_best_server()
-                s.download()
-                s.upload()
-                res = s.results.dict()
-                     
-            print hostname, 'ok'
             with open("ok.txt", "a") as myfile:
-                myfile.write(str(datetime.now())+'\n    -->'+hostname+'-->up\n')
-                if hostname == '8.8.8.8' :              
-                    myfile.write('  Ping:      '+str(res["ping"])+'ms \n')
-                    myfile.write('  Download:  '+str(res["download"]/1024)+'Kb/s \n')
-                    myfile.write('  Upload:    '+str(res["upload"]/1024)+'Kb/s \n\n\n')
-            print 'sleeping 5 minutes'
-            time.sleep(5*60)
+                myfile.write(str(datetime.now())+'    --> Ping: '+hostname+'-->up\n')
+            if hostname == '8.8.8.8':
+				with open("ok.txt", "a") as myfile:
+					myfile.write('\n\n')  
+				print 'sleeping 10 minutes'				
+				time.sleep(10*60)
         else:
             print hostname, 'DOWN'
             with open("down.txt", "a") as myfile:
-                myfile.write(str(datetime.now())+'\n    -->'+hostname+'-->pad veze \n')
-            print 'sleeping 0,5 minutes'
-            time.sleep(30)
+                myfile.write(str(datetime.now())+'    -->Ping: '+hostname+'-->pad veze \n')
+                if hostname == '8.8.8.8':
+                    myfile.write('\n\n')
+		   
+ 
+			
 	
